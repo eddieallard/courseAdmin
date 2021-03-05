196df7d547ef0896f49efd77ddd73da1ede8388e
Eddie Allard <eddie@theallardcompany.com>
2021-03-05 10:40:28 -0500

    imported toast and success message to handlesubmit event

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import CourseForm from './CourseForm';
 import * as courseApi from "../api/courseApi";
+import { toast } from 'react-toastify';
 
 const ManageCoursePage = props => {
     const [course, setCourse] = useState({
@@ -21,7 +22,10 @@ const ManageCoursePage = props => {
 
     function handleSubmit(event) {
         event.preventDefault();
-        courseApi.saveCourse(course);
+        courseApi.saveCourse(course).then( () => {
+            props.history.push("/courses");
+            toast.success("Course Saved");
+        });
     }
         
     return (
