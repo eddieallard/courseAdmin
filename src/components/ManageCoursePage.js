78c25b1705c6a3d87a60a59c5d798d80ab188644
Eddie Allard <eddie@theallardcompany.com>
2021-03-09 14:22:48 -0500

    added use Effect for dependancy array

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CourseForm from './CourseForm';
 import * as courseApi from "../api/courseApi";
 import { toast } from 'react-toastify';
 
 const ManageCoursePage = props => {
+    const [errors, setErrors] = useState({});
     const [course, setCourse] = useState({
         id: null,
         slug: "",
@@ -13,6 +14,13 @@ const ManageCoursePage = props => {
 
     });
 
+    useEffect( () => {
+        const slug = props.match.params.slug; //from the path '/course/:slug'
+        if (slug) {
+            courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+        }
+    }, [props.match.params.slug]);
+
     function handleChange({ target }) { 
         setCourse({
             ...course, 
@@ -20,8 +28,21 @@ const ManageCoursePage = props => {
         });
     }
 
+    function formIsValid() {
+        const _errors = {};
+
+        if (!course.title) _errors.title = "Title is required";
+        if (!course.authorId) _errors.authorId = "Author is required";
+        if (!course.category) _errors.category = "Category is required";
+
+        setErrors(_errors);
+        // Form is valid if the errors object has no properties
+        return Object.keys(_errors).length === 0;
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
+        if (!formIsValid()) return;
         courseApi.saveCourse(course).then( () => {
             props.history.push("/courses");
             toast.success("Course Saved");
@@ -31,7 +52,8 @@ const ManageCoursePage = props => {
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm 
+            <CourseForm
+            errors={errors}
             course={course} 
             onChange={handleChange} 
             onSubmit={handleSubmit} 
