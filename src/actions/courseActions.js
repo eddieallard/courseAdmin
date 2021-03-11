5e724ee949002d6e1293fb439d884c6ee2f1801a
Eddie Allard <eddie@theallardcompany.com>
2021-03-11 15:20:36 -0500

    created ActionTypes and callback, also created constants file

---
+++ src/actions/courseActions.js
@@ -0,0 +1,14 @@
+import dispatcher from '../appDispatcher';
+import * as courseApi from '../api/courseApi';
+import actionTypes from './actionTypes';
+
+
+export function saveCourse(course) {
+    return courseApi.saveCourse(course).then(savedCourse => {
+        //Hey dispatcher, go tell all the stor4es that a course was just created.
+        dispatcher.dispatch({
+            actionType: actionTypes.CREATE_COURSE,
+            course: savedCourse
+    });
+    })
+}
