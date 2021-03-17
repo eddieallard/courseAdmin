c9ae4a38866ba424700d402a6df2e64297419f95
Eddie Allard <eddie@theallardcompany.com>
2021-03-17 15:06:17 -0400

    added register function for CREATE_COURSE action type

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,6 +1,9 @@
 import { EventEmitter } from 'events'; 
+import { Dispatcher } from '../appDispatcher';
+import actionTypes from '../actions/actionTypes';
 
 const CHANGE_EVENT = "change";
+let _courses = [];
 
 class CourseStore extends EventEmitter {
   addChangeListerner(callback) {
@@ -17,4 +20,16 @@ class CourseStore extends EventEmitter {
 }
 
 const store = CourseStore();
+
+Dispatcher.register(action => {
+    switch(action.actionTypes) {
+        case actionTypes.CREATE_COURSE:
+            _courses.push(action.course);
+            store.emitChange();
+            break;
+        default:
+        // Nothing to do here
+    }
+})
+
 export default store;
