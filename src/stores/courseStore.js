0a1b12d7e38f2c093f5d3ee21dc3cce2679ed494
Eddie Allard <eddie@theallardcompany.com>
2021-03-23 18:55:20 -0400

    added new action type to dispatcher register for load courses and included constant file for load course.

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -2,6 +2,7 @@ import { EventEmitter } from 'events';
 import Dispatcher from "../appDispatcher";
 import actionTypes from '../actions/actionTypes';
 
+
 const CHANGE_EVENT = "change";
 let _courses = [];
 
@@ -33,6 +34,10 @@ Dispatcher.register(action => {
     switch(action.actionType) {
         case actionTypes.CREATE_COURSE:
             _courses.push(action.course);
+            store.emitChange();
+            break;
+        case actionTypes.LOAD_COURSES:
+            _courses = action.courses;
             store.emitChange();
             break;
         default:
