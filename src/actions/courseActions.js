21160b73abd086b56fd79b79cc27415afc370943
Eddie Allard <eddie@theallardcompany.com>
2021-03-23 23:48:30 -0400

    added delete button, DELETE_COURSE variable, DELETE COURSE case for dispatcer register and deleteCourse function for courseAction

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -19,7 +19,16 @@ export function loadCourses() {
     return courseApi.getCourses().then(courses => {
         dispatcher.dispatch({
             actionType: actionTypes.LOAD_COURSES,
-            courses: courses
+            courses
+    });
+    });
+}
+
+export function deleteCourse(id) {
+    return courseApi.deleteCourse(id).then(() => {
+        dispatcher.dispatch({
+            actionType: actionTypes.DELETE_COURSE,
+            id: id
     });
     });
 }
